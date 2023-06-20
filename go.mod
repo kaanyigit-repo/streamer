9f63f995b1201ce30ba78756824be2ad3dd97eda
kaanyigit-repo <kyigit2@illinois.edu>
2023-06-20 18:39:27 +0300

    removed terminal comands

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module main
 
 go 1.14
+
+require github.com/kkdai/youtube/v2 v2.8.1 // indirect
--- main.go
+++ main.go
@@ -2,20 +2,86 @@ package main
 
 import (
 	"fmt"
+	"github.com/kkdai/youtube/v2"
 	"io"
 	"log"
-	"net/http"
+	"net/url"
 	"os"
-	"os/exec"
+	"strings"
 )
 
 func main() {
+	var url string
+	greet(url)
+
+}
+func greet(web string) {
+
+	fmt.Println("Input the download link: ")
+	fmt.Scanln(&web)
+	if strings.Contains(web, "youtube") {
+		ExampleClient(web)
+		//startserver()
+		//openBrowser(web)
+	}
+
+}
+func ExampleClient(web string) {
+	u, err := url.Parse(web)
+	if err != nil {
+		log.Fatal(err)
+	}
+	videoID := u.Query().Get("v")
+	client := youtube.Client{}
+
+	video, err := client.GetVideo(videoID)
+	if err != nil {
+		panic(err)
+	}
+
+	formats := video.Formats.WithAudioChannels()
+	stream, _, err := client.GetStream(video, &formats[0])
+	if err != nil {
+		panic(err)
+	}
+
+	file, err := os.Create(videoID + ".mp4")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, stream)
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+/*
+func startserver() {
 	listenAddr := "localhost"
 	addr := listenAddr + `:` + "8080"
-
 	http.HandleFunc("/watch", stream)
 	log.Printf("starting server at %s", addr)
 	log.Fatal(http.ListenAndServe(addr, nil))
+}
+func openBrowser(web string) {
+	u, err := url.Parse(web)
+	if err != nil {
+		log.Fatal(err)
+	}
+	query := u.Query()
+	videoID := query.Get("v")
+	url := fmt.Sprintf("localhost:8080/watch?v=" + videoID)
+
+	// Command to open the link in the default browser
+	cmd := exec.Command("xdg-open", url) // For Linux
+
+	err2 := cmd.Start()
+	if err2 != nil {
+		log.Fatal(err)
+	}
 }
 
 func stream(w http.ResponseWriter, r *http.Request) {
@@ -26,26 +92,24 @@ func stream(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err := downloadVideoAndExtractAudio(v, w)
+	err := dl(v, w)
 	if err != nil {
 		log.Printf("stream error: %v", err)
 		fmt.Fprintf(w, "stream error: %v", err)
 		return
 	}
 }
-func downloadVideoAndExtractAudio(id string, out io.Writer) error {
+func dl(id string, out io.Writer) error {
 	url := fmt.Sprintf("https://youtube.com/watch?v=" + id)
-	//ytdl := exec.Command("youtube-dl", id)
 
-	ytdl := exec.Command("youtube-dl", url, "-o", "-")
+	ytdl := exec.Command("youtube-dl", url, "-o-")
 	ytdl.Stdout = out
-	ytdl.Stderr = os.Stderr // show progress
+	ytdl.Stderr = os.Stderr
 
 	err := ytdl.Run()
-	if err != nil {
-		log.Fatal(err)
-	}
 	log.Printf("stream finished")
-
 	return err
 }
+
+
+*/
