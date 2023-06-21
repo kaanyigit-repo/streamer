5d388ec3e78520136b2132f86aa9ec311d341755
kaanyigit-repo <kyigit2@illinois.edu>
2023-06-22 00:57:17 +0300

    service mod

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module main
 
 go 1.14
 
-require github.com/kkdai/youtube/v2 v2.8.1 // indirect
+require (
+	github.com/kkdai/youtube/v2 v2.8.1 // indirect
+	github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8 // indirect
+	github.com/playwright-community/playwright-go v0.2000.1 // indirect
+)
--- main.go
+++ main.go
@@ -5,111 +5,93 @@ import (
 	"github.com/kkdai/youtube/v2"
 	"io"
 	"log"
+	"net/http"
 	"net/url"
 	"os"
 	"strings"
 )
 
+var Addr string
+
 func main() {
-	var addr string
-	greet(addr)
-
+	greet()
 }
-func greet(web string) {
 
+func greet() {
 	fmt.Println("Input the download link: ")
-	fmt.Scanln(&web)
-	if strings.Contains(web, "youtube") {
-		ExampleClient(web)
-		//startserver()
-		//openBrowser(web)
-	}
 
+	fmt.Scanln(&Addr)
+
+	if strings.Contains(Addr, "youtube") {
+
+		streamServer()
+	}
 }
-func ExampleClient(web string) {
-	u, err := url.Parse(web)
+
+func streamServer() {
+	http.HandleFunc("/stream", streamBot)
+	log.Printf("starting server at %s", "localhost:8080")
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
+func streamBot(w http.ResponseWriter, r *http.Request) {
+
+	u, err := url.Parse(Addr)
 	if err != nil {
 		log.Fatal(err)
 	}
 	videoID := u.Query().Get("v")
+
 	client := youtube.Client{}
 
 	video, err := client.GetVideo(videoID)
 	if err != nil {
-		panic(err)
+		log.Println("Error1:", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
 	}
 
 	formats := video.Formats.WithAudioChannels()
+	fmt.Println("check1")
+
 	stream, _, err := client.GetStream(video, &formats[0])
 	if err != nil {
-		panic(err)
+		log.Println("Error2:", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
 	}
+	_, err = io.Copy(w, stream)
 
+	fmt.Println("check2")
 	file, err := os.Create(videoID + ".mp4")
 	if err != nil {
-		panic(err)
+		log.Println("Error:", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
 	}
+	fmt.Println("check3")
 	defer file.Close()
 
 	_, err = io.Copy(file, stream)
+
 	if err != nil {
-		panic(err)
+		log.Println("Error3:", err)
 	}
+	fmt.Println("check4")
 
-}
+	/*
+		netErr, ok := err.(net.Error)
+		if ok && netErr.Timeout() {
+			log.Println("Client disconnected")
+			return
+		}
+	*/
 
-/*
-func startserver() {
-	listenAddr := "localhost"
-	addr := listenAddr + `:` + "8080"
-	http.HandleFunc("/watch", stream)
-	log.Printf("starting server at %s", addr)
-	log.Fatal(http.ListenAndServe(addr, nil))
-}
-func openBrowser(web string) {
-	u, err := url.Parse(web)
 	if err != nil {
-		log.Fatal(err)
-	}
-	query := u.Query()
-	videoID := query.Get("v")
-	url := fmt.Sprintf("localhost:8080/watch?v=" + videoID)
-
-	// Command to open the link in the default browser
-	cmd := exec.Command("xdg-open", url) // For Linux
-
-	err2 := cmd.Start()
-	if err2 != nil {
-		log.Fatal(err)
-	}
-}
-
-func stream(w http.ResponseWriter, r *http.Request) {
-	v := r.URL.Query().Get("v")
-	if v == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(w, "use format /watch?v=...")
+		log.Println("Error4:", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 
-	err := dl(v, w)
-	if err != nil {
-		log.Printf("stream error: %v", err)
-		fmt.Fprintf(w, "stream error: %v", err)
-		return
-	}
+	log.Println("Streaming completed")
 }
-func dl(id string, out io.Writer) error {
-	url := fmt.Sprintf("https://youtube.com/watch?v=" + id)
-
-	ytdl := exec.Command("youtube-dl", url, "-o-")
-	ytdl.Stdout = out
-	ytdl.Stderr = os.Stderr
-
-	err := ytdl.Run()
-	log.Printf("stream finished")
-	return err
-}
-
-
-*/
